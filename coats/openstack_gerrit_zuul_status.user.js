92d12256a7f359646df5fcefd40f3aa48cf63e2a
Sorin Sbarnea <ssbarnea@redhat.com>
2018-10-10 12:48:52 +0100

    POC: add support for multiple zuul servers
    Query both zuul.openstack.org and softwarefactory-project.io for
    status of the jobs and builds a consolidated result table.
    
    CHANGE IS NOT FULLY WORKING, FEEL FREE TO FIX IT.
    
    Bugs:
    - reloads entire page after number of seconds (not clear why)
    - fails to perform correct AJAX query when current browser page
    does not contain current changeset as it would use "0" insteaf of
    correct number. The result is that Zuul will return nothing if
    the changeset version is not the one currently being build. Need
    to find alternative way to obtain it as the URL is not guaranteed to
    have it.
    
    Change-Id: Ifc89e456de88f77baa7ca772a852ad8e8d059d57

--- coats/openstack_gerrit_zuul_status.user.js
+++ coats/openstack_gerrit_zuul_status.user.js
@@ -24,117 +24,165 @@
 
 // Config
 
-const zuul_status_base = "https://zuul.openstack.org/";
-const zuul_status_url = zuul_status_base + "api/status/change/";
-
+// loads jQuery with a callback when jQuery has loaded
+function addJQuery(callback) {
+    var script = document.createElement("script");
+    script.setAttribute("src", "https://code.jquery.com/jquery-3.3.1.min.js");
+    script.addEventListener("load", function() {
+        var script = document.createElement("script");
+        script.textContent = "$=jQuery.noConflict(true);(" + callback.toString() + ")();";
+        document.body.appendChild(script);
+    }, false);
+    // document.body.appendChild(script);
+}
 // /Config
 
 // Script start
 
 /* defined by hideci.js */
 /* global ci_latest_patchset, ci_parse_comments */
+function main() {
+    const zuul_servers = {
+        "openstack": {
+            "zuul_status_base": "https://zuul.openstack.org/",
+            "zuul_api": "api/status/change/" },
+        "rdo": {
+            "zuul_status_base": "https://softwarefactory-project.io/zuul/api/tenant/rdoproject.org/",
+            "zuul_api": "status/change/" }
+    };
+    // eslint-disable-next-line no-undef
+    $("style#gerrit_sitecss").append(".result_RUNNING { color: #1e9ced; }");
 
-$("style#gerrit_sitecss").append(".result_RUNNING { color: #1e9ced; }");
+    var render = function(jobs) {
+        // eslint-disable-next-line no-undef
+        var location = $("table.test_result_table");
 
-var render = function(jobs) {
-    var location = $("table.test_result_table");
+        var table = "<tbody>";
 
-    var table = "<tbody>" +
-      "<tr>" +
-      "<td class=\"header\">Zuul check</td>" +
-      "<td class=\"header ci_date result_WARNING\">Still running</td>" +
-      "</tr>";
+        for(var server in jobs) {
+            table += "<tr>" +
+                "<td class=\"header\">Zuul check on " + server + "</td>" +
+                "<td class=\"header ci_date result_WARNING\">Still running</td>" +
+                "</tr>";
 
-    $.each(jobs, function(i, job) {
-        var status_with_completeness = ((job.status === "running" && typeof job.completeness !== "undefined") ? "RUNNING (" + job.completeness + ")" : job.status.toUpperCase());
-        var voting = job.voting === true ? "" : "<small> (non-voting)</small>";
+            for (var i in jobs[server]) {
+                if (jobs[server].hasOwnProperty(i)) {
+                    var job = jobs[server][i];
 
-        table += "<tr>" +
-      "<td><a href=\"" + job.url + "\" rel=\"nofollow\">" + job.name + "</a>" + voting + "</td>" +
-      "<td><span class=\"comment_test_result\"><span class=\"result_" + job.status.toUpperCase() +"\">" + status_with_completeness + "</span></td>" +
-      "</tr>";
-    });
+                    var status_with_completeness = ((job.status === "running" && typeof job.completeness !== "undefined") ? "RUNNING (" + job.completeness + ")" : job.status.toUpperCase());
+                    var voting = job.voting === true ? "" : "<small> (non-voting)</small>";
 
-    table += "</tbody>";
-
-    location.html(table);
-};
-
-var main = function() {
-    const url = $(location).attr("href");
-    const matches_url = /^https?:\/\/review\.openstack\.org\/(#\/c\/)?(\d*)\/?(\d*)?\/?$/.exec(url);
-
-    const change_id = matches_url[2];
-    var change_ver = matches_url[3];
-
-    if (typeof change_ver === "undefined"){
-        change_ver = ci_latest_patchset(ci_parse_comments());
-    }
-
-    var status_url = zuul_status_url + change_id + "," + change_ver;
-
-
-    $.getJSON(status_url, function(data) {
-        var queue;
-        var jobs = [];
-
-        if (data.length === 0){
-            if ($(".result_WARNING").length > 0){
-                location.reload();
-            }
-            return;
-        }
-
-        for(var i=0; i <= data.length; i++){
-            queue = data[i];
-            if (queue.items_behind.length == 0){
-                break;
-            }
-        }
-
-        if (!queue){
-            console.log("couldn't find a queue");
-            return;
-        }
-
-        $.each(queue.jobs, function(i, job) {
-            var item = {};
-
-            item.status = job.result ? job.result.toLowerCase() : (job.url ? "running" : "queued");
-            item.name = job.name;
-            item.voting = job.voting;
-            item.pipeline = job.pipeline;
-            item.url = job.result ? job.report_url : (job.url ? zuul_status_base + job.url : "#");
-
-            if (item.status === "running" && job.remaining_time !== null){
-                item.completeness = Math.round(100 * (job.elapsed_time / (job.elapsed_time + job.remaining_time))) + "%";
+                    table += "<tr>" +
+                        "<td><a href=\"" + job.url + "\" rel=\"nofollow\">" + job.name + "</a>" + voting + "</td>" +
+                        "<td><span class=\"comment_test_result\"><span class=\"result_" + job.status.toUpperCase() + "\">" + status_with_completeness + "</span></td>" +
+                        "</tr>";
+                }
             }
 
-            jobs.push(item);
+        }
 
+        table += "</tbody>";
+
+        location.html(table);
+    };
+
+    var main_loop = function(jobs_dict = {}) {
+        // eslint-disable-next-line no-undef
+        const url = $(location).attr("href");
+        const matches_url = /^https?:\/\/review\.openstack\.org\/(#\/c\/)?(\d*)\/?(\d*)?\/?$/.exec(url);
+
+        if(!matches_url) return;
+        const change_id = matches_url[2];
+        var change_ver = matches_url[3];
+
+        if (typeof change_ver === "undefined"){
+            // eslint-disable-next-line no-undef
+            change_ver = ci_latest_patchset(ci_parse_comments());
+        }
+
+        for(var zuul in zuul_servers) {
+
+            var zuul_status_base = zuul_servers[zuul]["zuul_status_base"];
+            var status_url = zuul_status_base + zuul_servers[zuul]["zuul_api"] + change_id + "," + change_ver;
+
+            // eslint-disable-next-line no-undef
+            $.getJSON(status_url, (function() {
+                var ii = zuul;
+                return function(data) {
+                    var queue;
+                    var jobs = [];
+
+                    if (data.length === 0){
+                        // eslint-disable-next-line no-undef
+                        if ($(".result_WARNING").length > 0){
+                            location.reload();
+                        }
+                        return;
+                    }
+                    for(var i=0; i <= data.length; i++){
+                        queue = data[i];
+                        if(!queue) {
+                            break;
+                        }
+                        if (queue.hasOwnProperty("items_behind") && queue.items_behind.length == 0){
+                            break;
+                        }
+                    }
+                    if (!queue){
+                        // eslint-disable-next-line no-console
+                        console.log("couldn't find a queue");
+                        return;
+                    }
+                    // eslint-disable-next-line no-undef
+                    $.each(queue.jobs, function(i, job) {
+                        var item = {};
+
+                        item.status = job.result ? job.result.toLowerCase() : (job.url ? "running" : "queued");
+                        item.name = job.name;
+                        item.voting = job.voting;
+                        item.pipeline = job.pipeline;
+                        item.url = job.result ? job.report_url : (job.url ? zuul_status_base + job.url : "#");
+
+                        if (item.status === "running" && job.remaining_time !== null){
+                            item.completeness = Math.round(100 * (job.elapsed_time / (job.elapsed_time + job.remaining_time))) + "%";
+                        }
+                        jobs.push(item);
+
+                    });
+
+                    jobs_dict[ii] = jobs;
+                    setTimeout(main_loop, 10000, jobs_dict);
+
+                };
+            })());
+
+        }
+        render(jobs_dict);
+    }; // main_loop
+
+
+    // So we refresh on each update.
+
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    // eslint-disable-next-line no-unused-vars
+    var observer = new MutationObserver(function(mutations, observer) {
+        // eslint-disable-next-line no-undef
+        var span = $("span.rpcStatus");
+        // eslint-disable-next-line no-undef
+        $.each(mutations, function(i, mutation) {
+            if (mutation.target === span[0] &&
+                mutation.attributeName === "style" &&
+                (!(span.is(":visible")))) {
+                main_loop();
+            }
         });
-
-        render(jobs);
-        setTimeout(main, 2000);
     });
-};
-
-
-// So we refresh on each update.
-
-var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-/* eslint no-unused-vars: 0 */
-var observer = new MutationObserver(function(mutations, observer) {
-    var span = $("span.rpcStatus");
-    $.each(mutations, function(i, mutation) {
-        if (mutation.target === span[0] &&
-        mutation.attributeName === "style" &&
-        (!(span.is(":visible")))) {
-            main();
-        }
+    observer.observe(document, {
+        subtree: true,
+        attributes: true
     });
-});
-observer.observe(document, {
-    subtree: true,
-    attributes: true
-});
+
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
